daa497dec4f8f8aa2764dabcdb1983a01be62bab
Rasmus Højte <Rasmus.Hoejte@Anticimex.com>
2025-02-20 10:20:51 +0100

    Add new Target

--- build/Build.cs
+++ build/Build.cs
@@ -4,12 +4,15 @@ using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.PathConstruction;
+
 
 [GitHubActions(
     "continuous",
@@ -18,13 +21,14 @@ using static Nuke.Common.IO.PathConstruction;
     InvokedTargets = new[] { nameof(Compile) })]
 class Build : NukeBuild
 {
+    [GitRepository] readonly GitRepository Repository;
+
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -46,4 +50,12 @@ class Build : NukeBuild
         {
         });
 
+    Target PrintSomeInformation => _ => _
+        .TriggeredBy(Compile)
+        .Executes(() =>
+        {
+            Log.Information("Commit = {Value}", Repository.Commit);
+            Log.Information("Branch = {Value}", Repository.Branch);
+            Log.Information("Tags = {Value}", Repository.Tags);
+        });
 }
